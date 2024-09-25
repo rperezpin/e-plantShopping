a776c4c83b80152ef1f1e43a8cc59c4d787795fa
rperezpin <123058849+rperezpin@users.noreply.github.com>
2024-09-25 13:43:25 +0200

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,20 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import { addItem } from './CartSlice';
+
+import ProductCard from './ProductCard';
+
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+
+    const cart = useSelector((state)=> state.cart.items);
+    const cartQuantity = useSelector(state=> state.cart.quantity);
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -232,20 +243,25 @@ function ProductList() {
     fontSize: '30px',
     textDecoration: 'none',
    }
-   const handleCartClick = (e) => {
-    e.preventDefault();
-    setShowCart(true); // Set showCart to true when cart icon is clicked
-};
-const handlePlantsClick = (e) => {
-    e.preventDefault();
-    setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
-    setShowCart(false); // Hide the cart when navigating to About Us
-};
+    const handleCartClick = (e) => {
+        e.preventDefault();
+        setShowCart(true); // Set showCart to true when cart icon is clicked
+    };
+    const handlePlantsClick = (e) => {
+        e.preventDefault();
+        setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
+        setShowCart(false); // Hide the cart when navigating to About Us
+    };
 
-   const handleContinueShopping = (e) => {
-    e.preventDefault();
-    setShowCart(false);
-  };
+    const handleContinueShopping = (e) => {
+        e.preventDefault();
+        setShowCart(false);
+    };
+
+    const handleAddToCart = (product)=> {
+        dispatch(addItem(product));
+    }
+
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -263,12 +279,44 @@ const handlePlantsClick = (e) => {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div>
+                    <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                        <h1 className='cart'>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                                <rect width="156" height="156" fill="none"></rect>
+                                <circle cx="80" cy="216" r="12"></circle>
+                                <circle cx="184" cy="216" r="12"></circle>
+                                <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                            </svg>
+                            <p className='cart_quantity_count'>{cartQuantity}</p>
+                        </h1>
+                    </a>
+                </div>
             </div>
         </div>
         {!showCart? (
         <div className="product-grid">
 
+            {plantsArray.map((item) => (
+                <>
+                    <h1 style={{
+                        margin: '30px',
+                        borderBlockStart: "solid",
+                        borderBlockEnd: "solid",
+                        writingMode: "horizontal-tb",
+                        borderBlockWidth: "2px",
+                        paddingInline: "50px"
+                    }}>{item.category}</h1>
+
+                    <div className='product-list'>
+                        {item.plants.map(( product )=> {
+                            const productInCart = (cart.length>0? cart.find((e)=> e.name==product.name) : undefined);
+                            const initialButtonState = productInCart === undefined;
+                            return <ProductCard product={product} dispatchAddItemCallback={handleAddToCart} initialButtonState={initialButtonState}/>
+                        })}
+                    </div>
+                </>
+            ))}
 
         </div>
  ) :  (
