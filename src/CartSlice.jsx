88d94b8e7877e259ed7ffa4b74cb91b2b834cf2d
rperezpin <123058849+rperezpin@users.noreply.github.com>
2024-09-25 13:44:16 +0200

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,16 +4,54 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    quantity: 0, 
   },
   reducers: {
     addItem: (state, action) => {
-    
+      state.items.push({...action.payload, quantity: 1});
+      state.quantity++;
     },
     removeItem: (state, action) => {
+      const productIndexInCartIndex = state.items.findIndex((item)=> item.name === action.payload.name);
+
+      var quantityToRemove = 0;
+
+      function deleteItem() {
+        quantityToRemove = state.items[productIndexInCartIndex].quantity;
+        state.items.splice(productIndexInCartIndex, 1);
+      }
+
+      if (productIndexInCartIndex !== -1) {
+        if (action.payload.delete === true) {
+          deleteItem();
+        } else {
+          if (state.items[productIndexInCartIndex].quantity>1) {
+            state.items[productIndexInCartIndex].quantity--;
+
+            quantityToRemove = 1;
+          } else {
+            deleteItem();
+          }
+        }
+
+        state.quantity -= quantityToRemove;
+      }
     },
     updateQuantity: (state, action) => {
+      // .findIndex returns -1 if the target element not found in array 
+      const productIndexInCart = state.items.findIndex((item)=> item.name === action.payload.name);
+      if (productIndexInCart !== -1) {
+        const productExistingState = state.items[productIndexInCart];
 
-    
+        const productQuantity = productExistingState.quantity+1;
+
+        state.items[productIndexInCart] = {
+          ...productExistingState,
+          quantity: productQuantity
+        };
+
+        state.quantity++;
+      }
     },
   },
 });
